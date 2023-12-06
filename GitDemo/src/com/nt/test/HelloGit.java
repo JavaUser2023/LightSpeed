b6ff3af7f00559fd40d8e4a13bc430a1681ff466
Admin <Admin@DESKTOP-EQ55Q8H>
2023-12-07 02:53:23 +0530

    HelloGit Modified

--- GitDemo/src/com/nt/test/HelloGit.java
+++ GitDemo/src/com/nt/test/HelloGit.java
@@ -1,10 +1,14 @@
 package com.nt.test;
 
+import java.time.LocalDateTime;
+
 public class HelloGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello"); 
+		LocalDateTime l=LocalDateTime.now();
+	System.out.println(l);
 	}
 
 }
